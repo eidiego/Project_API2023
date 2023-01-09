6e4ca15bdc3935afe1b6c43ff3fa5bb101eda3a9
Diego R. Barbosa <diegokod@gmail.com>
2023-01-09 20:48:11 -0300

    Feat: EndPoint findAll by user

--- src/main/java/br/com/eidiego/api/services/UserService.java
+++ src/main/java/br/com/eidiego/api/services/UserService.java
@@ -2,8 +2,12 @@ package br.com.eidiego.api.services;
 
 import br.com.eidiego.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+
+    List<User> findAll();
 
 }
