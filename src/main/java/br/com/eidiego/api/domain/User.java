8cc46e0f155c8c645d49c437a7c98f3a0493c953
Diego R. Barbosa <diegokod@gmail.com>
2023-01-09 18:48:12 -0300

    Feat: User entity created

---
+++ src/main/java/br/com/eidiego/api/domain/User.java
@@ -0,0 +1,36 @@
+package br.com.eidiego.api.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
