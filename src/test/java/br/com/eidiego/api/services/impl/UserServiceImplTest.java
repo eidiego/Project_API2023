e2ed7ad695647ce4620cfa7c8b8a5a1fb98854d4
Diego R. Barbosa <diegokod@gmail.com>
2023-01-09 22:03:18 -0300

    Test: Create initial tests for serviceimpl

---
+++ src/test/java/br/com/eidiego/api/services/impl/UserServiceImplTest.java
@@ -0,0 +1,78 @@
+package br.com.eidiego.api.services.impl;
+
+import br.com.eidiego.api.domain.User;
+import br.com.eidiego.api.domain.dto.UserDTO;
+import br.com.eidiego.api.repositories.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+@SpringBootTest
+class UserServiceImplTest {
+
+    public static final String NAME = "diego";
+    public static final Integer ID = 1;
+    public static final String EMAIL = "diego@mail.com";
+    public static final String PASSWORD = "123";
+    @InjectMocks
+    private UserServiceImpl service;
+
+    @Mock
+    private UserRepository repository;
+
+    @Mock
+    private ModelMapper mapper;
+
+    private User user;
+    private UserDTO userDTO;
+    private Optional<User> optionalUser;
+
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void whenFindByIdThenReturnAnUserInstance() {
+
+        Mockito.when(repository.findById(Mockito.anyInt())).thenReturn(optionalUser);
+
+        User response = service.findById(ID);
+
+        Assertions.assertEquals(User.class, response.getClass());
+
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void startUser() {
+        user = new User(ID, NAME, EMAIL, PASSWORD);
+        userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+        optionalUser = Optional.of(new User(ID, NAME, EMAIL, PASSWORD));
+    }
+
+}
