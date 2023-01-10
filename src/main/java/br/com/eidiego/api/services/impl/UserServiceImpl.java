fd15a7f952f61aa0ad0fb1c25782ab1a31149d91
Diego R. Barbosa <diegokod@gmail.com>
2023-01-09 21:12:20 -0300

    Feat: Create save method and create exceptions to validate if email already exist

--- src/main/java/br/com/eidiego/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/eidiego/api/services/impl/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package br.com.eidiego.api.services.impl;
 
 import br.com.eidiego.api.domain.User;
+import br.com.eidiego.api.domain.dto.UserDTO;
 import br.com.eidiego.api.repositories.UserRepository;
 import br.com.eidiego.api.services.UserService;
+import br.com.eidiego.api.services.exceptions.DataIntegratyViolationException;
 import br.com.eidiego.api.services.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +15,9 @@ import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private ModelMapper mapper;
 
     @Autowired
     private UserRepository repository;
@@ -25,4 +31,19 @@ public class UserServiceImpl implements UserService {
     public List<User> findAll() {
        return repository.findAll();
     }
+
+    @Override
+    public User create(UserDTO obj) {
+        findByEmail(obj);
+        return repository.save(mapper.map(obj, User.class));
+    }
+
+    private void findByEmail(UserDTO obj) {
+        Optional<User> user = repository.findByEmail(obj.getEmail());
+        if(user.isPresent()) {
+            throw new DataIntegratyViolationException("E-mail já cadastrado no sistema!");
+        }
+    }
+
+
 }
